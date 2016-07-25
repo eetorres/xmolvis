f23334d2a0339d3ae89703d7f432d887bb028640
eetorres <eetorres@gmail.com>
2016-07-24 20:24:45 -0400

    version counter

--- include/version.h
+++ include/version.h
@@ -5,7 +5,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
+	static const char DATE[] = "24";
 	static const char MONTH[] = "07";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] = "16.07";
@@ -21,13 +21,13 @@ namespace AutoVersion{
 	static const long REVISION = 0;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3;
+	static const long BUILDS_COUNT = 4;
 	#define RC_FILEVERSION 0,1,0,0
-	#define RC_FILEVERSION_STRING "0, 1, 0, 0\3"
+	#define RC_FILEVERSION_STRING "0, 1, 0, 0\4"
 	static const char FULLVERSION_STRING [] = "0.1.0.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 3;
+	static const long BUILD_HISTORY = 4;
 	
 
 }
